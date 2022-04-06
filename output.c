4b4113ce353cd14fb8054b89150a72266f51536a
Tyler887 <tylermageeshields@gmail.com>
2022-04-06 20:17:22 +0100

    Update output.c

--- output.c
+++ output.c
@@ -4,11 +4,11 @@ test their code!
 */
 #include <stdio.h>
 
-int main() {
-  if ( argc[1] == 0 ) {
-    printf("output: no args");
-    return 1;
-  }
-  printf(argv[1]);
-  return 0;
+int main(int argc, char* argv[] ) {
+    if ( argv[1] == 0 ) {
+      printf("output: no args");
+      return 1;
+    }
+    printf(argv[1]);
+    return 0;
 }
