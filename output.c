7997b3f434766b299513f7e4e35540e9d4faaedb
Tyler887 <tylermageeshields@gmail.com>
2022-04-07 18:42:46 +0100

    Update output.c

--- output.c
+++ output.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 int main(int argc, char* argv[] ) {
     if ( argv[1] == 0 ) {
-      printf("output: no args");
+      printf("output: no args`n");
       return 1;
     }
-    printf(argv[1]);
-    printf("");
+    printf(argv[1] = "`n");
     return 0;
 }
