6c322956fd0b962390152514b1e494423b7d2a48
Tyler887 <tylermageeshields@gmail.com>
2022-04-07 18:38:04 +0100

    Update output.c

--- output.c
+++ output.c
@@ -1,9 +1,4 @@
-/*
-I mainly do the build on GCC: gcc output.c. Developers should
-test their code!
-*/
 #include <stdio.h>
-
 int main(int argc, char* argv[] ) {
     if ( argv[1] == 0 ) {
       printf("output: no args");
